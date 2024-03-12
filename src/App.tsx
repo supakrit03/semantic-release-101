0047aa32b348370ddea7b11811605a6551646ba7
Supakrit <supakrit@enres.co>
2024-03-12 23:16:28 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>A</h1>
+      <h1>B fixed</h1>
+      <h1>B hot fixed</h1>
+      <h1>add text eiei 123</h1>
+      <h1>add text eiei 123</h1>
+      <h1>add feat add text</h1>
+      <h1>add feat add 5</h1>
+      <h1>add feat a</h1>
+    </>
+  );
+}
+
+export default App;
